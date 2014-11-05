8f5917db9ffebee678baa036edbb6b92c547c617
Mitar <mitar.git@tnode.com>
2014-11-05 14:36:52 -0800

    Initial version.

---
+++ test-methods.js
@@ -0,0 +1,23 @@
+var Collections = new Meteor.Collection('Collections');
+
+Meteor.methods({
+  testMethod: function (id) {
+    Collections.insert({collection: id});
+  }
+});
+
+if (Meteor.isClient) {
+  Template.hello.events({
+    'click button': function () {
+      Meteor.call('testMethod', Random.id(), function (error) {
+        if (error) console.error(error);
+      });
+    }
+  });
+
+  Collections.find().observe({
+	added: function (document) {
+      new Meteor.Collection('collections.' + document.collection);
+	}
+  });
+}
